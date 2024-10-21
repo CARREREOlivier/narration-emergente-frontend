5e8b1f0ea9b2b4b71054b8d518d865451fa29139
CARREREOlivier <carrereolivier@yahoo.fr>
2024-10-21 12:22:36 +0200

    Fix: Update AuthService to store user in localStorage and handle logout properly

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
+import {User} from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +35,9 @@ export class AuthService {
         localStorage.removeItem('user');  // Supprimer l'utilisateur après déconnexion
       })
     );
+  }
+
+  register(user: User) {
+
   }
 }
