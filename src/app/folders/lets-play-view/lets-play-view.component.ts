c95b9759048ced792afbf1c5804d4d9363b2cd48
CARREREOlivier <carrereolivier@yahoo.fr>
2024-10-29 15:07:28 +0100

    Fix: Lets play-view corrected

--- src/app/folders/lets-play-view/lets-play-view.component.ts
+++ src/app/folders/lets-play-view/lets-play-view.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { LetsPlaysService } from '../services/lets-plays.service';
-import { LetsPlay } from '../models/lets-play.model';
+import { LetsPlaysService } from '../../services/lets-plays.service';
+import { LetsPlay } from '../../models/lets-play.model';
+import {DatePipe, NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-lets-play-view',
   templateUrl: './lets-play-view.component.html',
   standalone: true,
+  imports: [
+    DatePipe,
+    NgForOf,
+    RouterLink
+  ],
   styleUrls: ['./lets-play-view.component.css']
 })
 export class LetsPlayViewComponent implements OnInit {
